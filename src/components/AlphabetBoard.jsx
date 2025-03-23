685c334150db59decdb075ef45a69cfcf94f75d8
Avijit Pandey <avijeetpandey87@gmail.com>
2025-03-23 14:05:24 +0530

    chore: re-enable shuffle for source letters

--- src/components/AlphabetBoard.jsx
+++ src/components/AlphabetBoard.jsx
@@ -72,10 +72,10 @@ const AlphabetBoard = ({languageName, vowels, consonants, onBack}) => {
     }));
 
     // // Fisher-Yates shuffle algorithm
-    // for (let i = letters.length - 1; i > 0; i--) {
-    //   const j = Math.floor(Math.random() * (i + 1));
-    //   [letters[i], letters[j]] = [letters[j], letters[i]];
-    // }
+    for (let i = letters.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [letters[i], letters[j]] = [letters[j], letters[i]];
+    }
 
     return letters;
   }
